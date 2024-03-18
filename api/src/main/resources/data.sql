3a6f443026533f46704324cf4ace8ed09eb2371c
murilob03 <muriloviskboccardo@gmail.com>
2024-03-18 16:31:22 -0300

    data injection script complete

--- api/src/main/resources/data.sql
+++ api/src/main/resources/data.sql
@@ -1,2 +1,53 @@
-INSERT INTO cliente 
-VALUES (0, "Igor da Penha", "12312312312", "igor@email.com", "$2a$10$l/vcP52EiUtLx1r3ZgwOXOve4lyOdz3okcMoFQ3pJ3N4YH1kx8oVC", "4412341234");
+INSERT INTO
+    cliente
+VALUES
+    (
+        0,
+        "Igor da Penha",
+        "12312312312",
+        "igor@email.com",
+        "$2a$10$l/vcP52EiUtLx1r3ZgwOXOve4lyOdz3okcMoFQ3pJ3N4YH1kx8oVC",
+        "4412341234"
+    );
+
+INSERT INTO
+    agente_imobiliario
+VALUES
+    (
+        1,
+        "Murilo Boccardo",
+        "45826234000120",
+        "12312312345",
+        "1234568",
+        "murilo@email.com",
+        "senhasenha",
+        "44123451234"
+    );
+
+INSERT INTO
+    agente_imobiliario
+VALUES
+    (
+        1,
+        "Rodrigo Wagatsuma da Silva",
+        "",
+        "12612312345",
+        "1234268",
+        "rodrigo@email.com",
+        "senhasenha",
+        "44163451234"
+    );
+
+INSERT INTO
+    agente_imobiliario
+VALUES
+    (
+        1,
+        "Julia Shimano",
+        "35244024000170",
+        "12312832345",
+        "1236468",
+        "julia@email.com",
+        "senhasenha",
+        "44111451234"
+    );
